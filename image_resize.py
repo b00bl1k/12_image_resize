234fdd1653350bed0de5cb6233ede451170640f0
Alexey Ryabov <6006l1k@gmail.com>
2019-02-27 23:00:29 +0300

    Pure functions

--- image_resize.py
+++ image_resize.py
@@ -7,42 +7,40 @@ from PIL import Image
 PROPORTION_ERR_MIN = 0.1
 
 
-def get_target_filename(source, target, size):
+def get_target_filename(source, target, width, height):
     if target:
         return target
     root, ext = os.path.splitext(source)
-    return "{}__{}x{}{}".format(root, *size, ext)
+    return "{}__{}x{}{}".format(root, width, height, ext)
 
 
-def resize_image(source, new_size, scale):
-    im = Image.open(source)
-    origin_width, origin_height = im.size
-    origin_prop = origin_width / origin_height
-
+def calculate_new_size(src_width, src_height, dst_width, dst_height, scale):
+    src_prop = src_width / src_height
     if scale:
-        new_width = int(origin_width * scale)
-        new_height = int(origin_height * scale)
-    elif new_size[0] is None:
-        new_width = int(new_size[1] * origin_prop)
-        new_height = new_size[1]
-    elif new_size[1] is None:
-        new_width = new_size[0]
-        new_height = int(new_size[0] / origin_prop)
+        new_width = int(src_width * scale)
+        new_height = int(src_height * scale)
+    elif dst_width is None:
+        new_width = int(dst_width * src_prop)
+        new_height = dst_width
+    elif dst_height is None:
+        new_width = dst_width
+        new_height = int(dst_width / src_prop)
     else:
-        new_width, new_height = new_size
-        new_prop = new_width / new_height
-        if abs(new_prop - origin_prop) > PROPORTION_ERR_MIN:
-            print("Warning: new image is not same proportion")
+        new_width = dst_width
+        new_height = dst_height
 
-    new_image = im.resize((new_width, new_height))
-    im.close()
+    return new_width, new_height
 
-    return new_image
+
+def is_proportional(src_width, src_height, new_width, new_height):
+    src_prop = src_width / src_height
+    new_prop = new_width / new_height
+    return abs(src_prop - new_prop) < PROPORTION_ERR_MIN
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Image resizer.")
-    parser.add_argument("source", help='Path to the source image')
+    parser.add_argument("source", help="Path to the source image")
     parser.add_argument(
         "target",
         nargs="?",
@@ -54,11 +52,21 @@ def parse_args():
 
     args = parser.parse_args()
 
-    if args.scale and args.scale <= 0:
-        raise ValueError("Invalid value of scale argument")
+    if args.scale is not None and args.scale <= 0:
+        parser.error("Invalid value of scale argument")
 
-    if bool(args.scale) == (bool(args.width) or bool(args.height)):
-        raise ValueError("Width/height or scale must be specified")
+    if args.width is not None and args.width <= 0:
+        parser.error("Invalid value of width option")
+
+    if args.height is not None and args.height <= 0:
+        parser.error("Invalid value of height option")
+
+    if args.scale is not None and (args.width is not None or
+                                   args.height is not None):
+        parser.error("Width/height and scale is exclusive options")
+
+    if not any((args.scale, args.width, args.height)):
+        parser.error("No resize options specified")
 
     return args
 
@@ -66,23 +74,30 @@ def parse_args():
 def main():
     try:
         args = parse_args()
-        img = resize_image(
-            args.source,
-            (args.width, args.height),
+        src_img = Image.open(args.source)
+        src_width, src_height = src_img.size
+        new_width, new_height = calculate_new_size(
+            src_width,
+            src_height,
+            args.width,
+            args.height,
             args.scale
         )
+        if not is_proportional(src_width, src_height, new_width, new_height):
+            print("Warning: target image is not proportional to source")
+        new_img = src_img.resize((new_width, new_height))
+        src_img.close()
         target_filename = get_target_filename(
             args.source,
             args.target,
-            img.size
+            src_width,
+            src_height
         )
-        img.save(target_filename)
+        new_img.save(target_filename)
     except FileNotFoundError:
         sys.exit("The source image not found")
     except OSError:
         sys.exit("The source is not valid image file")
-    except ValueError as e:
-        sys.exit(str(e))
 
 
 if __name__ == "__main__":
